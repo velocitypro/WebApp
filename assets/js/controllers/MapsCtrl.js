0d05f6341b9c0f6c76b589472fe4da6119695950
artemb89 <artem_b89@mail.ru>
2015-06-24 12:44:26 +0300

    Редактор маршрута
    Редактирование и создание маршрута.

--- assets/js/controllers/MapsCtrl.js
+++ assets/js/controllers/MapsCtrl.js
@@ -1,78 +1,71 @@
 define(function() {
     return ['$scope', '$http', function($scope, $http) {
 
-	$scope.message="Ya-maps test!";
-	$scope.geoObjects=[
-    {
-        geometry: {
-            type: 'LineString',
-            coordinates: [
-                [37.50,55.80],
-                [37.40,55.70]
-            ]
-        },
-        properties: {
-            hintContent: "Я геообъект",
-            balloonContent: "Меня можно перетащить"
-        }
-    }];
+	$scope.routeTitle="";
+	$scope.routeDescription="";
+	var _route=null;
 	var routePoints = [
-		'Москва, улица Крылатские холмы',
-		{
-			point: 'Москва, метро Молодежная',
-			// метро "Молодежная" - транзитная точка
-			// (проезжать через эту точку, но не останавливаться в ней).
-			type: 'viaPoint'
-		},
-		[55.731272, 37.447198], // метро "Кунцевская".
-		'Москва, метро Пионерская'
 	];
+	var startEditing=false;
+	$scope.btnLabel = 'Включить редактор маршрута';
+	$scope.SaveRoute= function()
+				{
+					$http.post('/Routes/create', {
+													title:$scope.routeTitle,
+													description:$scope.routeDescription,
+													waypoints:routePoints
+												}).
+					  success(function(data, status, headers, config) {
+						alert('Route created!');
+					  }).
+					  error(function(data, status, headers, config) {
+						alert('Error!');
+					  });
+				};
 	$scope.route = function(map){
-			ymaps.route(routePoints).then(function (route) {
-			map.geoObjects.add(route);
-			// Зададим содержание иконок начальной и конечной точкам маршрута.
-			// С помощью метода getWayPoints() получаем массив точек маршрута.
-			// Массив транзитных точек маршрута можно получить с помощью метода getViaPoints.
-			var points = route.getWayPoints(),
-				lastPoint = points.getLength() - 1;
-			// Задаем стиль метки - иконки будут красного цвета, и
-			// их изображения будут растягиваться под контент.
-			points.options.set('preset', 'twirl#redStretchyIcon');
-			// Задаем контент меток в начальной и конечной точках.
-			points.get(0).properties.set('iconContent', 'Точка отправления');
-			points.get(lastPoint).properties.set('iconContent', 'Точка прибытия');
-
-			// Проанализируем маршрут по сегментам.
-			// Сегмент - участок маршрута, который нужно проехать до следующего
-			// изменения направления движения.
-			// Для того, чтобы получить сегменты маршрута, сначала необходимо получить
-			// отдельно каждый путь маршрута.
-			// Весь маршрут делится на два пути:
-			// 1) от улицы Крылатские холмы до станции "Кунцевская";
-			// 2) от станции "Кунцевская" до "Пионерская".
-
-			var way,
-				segments,
-				path=['Трогаемся'];
-			// Получаем массив путей.
-			for (var i = 0; i < route.getPaths().getLength(); i++) {
-				way = route.getPaths().get(i);
-				segments = way.getSegments();
-				for (var j = 0; j < segments.length; j++) {
-					var street = segments[j].getStreet();
-					path.push('Едем ' + segments[j].getHumanAction() +
-						(street ? ' на ' + street : '') +
-						', проезжаем ' + segments[j].getLength() + ' м.,'
-					);
+		$scope.routeEdit = function()
+		{
+			if (!startEditing) {
+						// Включаем редактор.
+						startEditing=!startEditing;						
+						$scope.btnLabel = 'Отключить редактор маршрута';
+					} else {
+						// Выключаем редактор.
+						startEditing=!startEditing;						
+						$scope.btnLabel='Включить редактор маршрута';
+					}			
+		};
+		 
+		 var addRoutePoint=function(e)
+		{
+			if(startEditing)
+			{
+				routePoints.push(e.get('coords'));
+				if(routePoints.length>1) 
+				{
+					if(_route) map.geoObjects.remove(_route);
+					ymaps.route(routePoints, {	mapStateAutoApply: true	})
+											  .then(function (route) {_route=route;map.geoObjects.add(route);});
 				}
-			}
-			path.push('Останавливаемся');
-			$scope.$apply(function(){
-				$scope.path = path;
-			});
-		}, function (error) {
-			alert('Возникла ошибка: ' + error.message);
-		});
+				else 
+				{
+					var mark=new ymaps.GeoObject({
+						geometry:   {
+										type: "Point",
+										coordinates: e.get('coords')
+									},
+                        properties: {
+										iconContent: '1'
+									}
+					});
+				map.geoObjects.add(mark);
+				}
+			};
+						
+		};
+		map.events.add('click', addRoutePoint, this);
 	};
+	
+	
     }]
 })
